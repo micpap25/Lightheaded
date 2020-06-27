45bfdc92c57a151cb76609b67358a26ca536d577
Melissa Ozcan <34689581+MelissaOzcan@users.noreply.github.com>
2020-06-27 17:19:38 -0400

    tutorial
    #blockyash

---
+++ Assets/Scripts/T1ButtonController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class T1ButtonController : MonoBehaviour
+{
+    public void Kill()
+    {
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
+    }
+}
---
+++ Assets/Scripts/T2ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T2ButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.gameObject.GetComponentInParent<Canvas>().gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (gameObject.GetComponent<PlayerController>().numBalloons == 1)
+        {
+            transform.gameObject.GetComponentInParent<Canvas>().gameObject.SetActive(true);
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
+    }
+}
