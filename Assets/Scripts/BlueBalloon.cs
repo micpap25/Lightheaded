d726a919cabd81942bd25ddaf8ed2fe9d1c87c01
Michael Papadopoulos <micpap25@gmail.com>
2020-06-26 22:45:06 -0400

    Added Cake Outline

--- Assets/Scripts/BlueBalloon.cs
+++ Assets/Scripts/BlueBalloon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BlueBalloon : MonoBehaviour, Collectable
 {
+    public void FixedUpdate()
+    {
+        //visual stuff while not collected, floating up and down, particles
+    }
     public void collide()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
---
+++ Assets/Scripts/Cake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cake : MonoBehaviour, Collectable
+{
+    public int transitionTo;
+    // Start is called before the first frame update
+
+    public void FixedUpdate()
+    {
+     //visual stuff while not collected, floating up and down, particles
+    }
+    public void collide()
+    {
+        StartCoroutine(Load());
+        //Visual stuff for loading!
+    }
+
+    private IEnumerator Load()
+    {
+        AsyncOperation async = Application.LoadLevelAsync("Level" + transitionTo);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
