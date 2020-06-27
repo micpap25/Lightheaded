f14ed9aa43a4b5c262ff2cb3487e93a109fa4e61
Yash Choksey <yascho21@bergen.org>
2020-06-27 15:04:12 -0400

    Menu, but there is actually things

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+
+
+  void Start () {
+
+  }
+
+  public void loadFirstLvl(){
+      Debug.Log ("You have clicked the NewGameButton!");
+      SceneManager.LoadScene("Level1");
+  }
+  public void QuitGame(){
+    Debug.Log("Haha loser you quit. jk lov u <3");
+    Application.Quit();
+
+  }
+}
