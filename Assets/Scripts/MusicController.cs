0a8087fa1e6a496d375328ddec5746c5054a6a8b
Michael Papadopoulos <micpap25@gmail.com>
2020-06-27 17:03:19 -0400

    Added Music, started enemy stuff

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private AudioSource aus;
+    private AudioClip currentClip;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        aus = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic(AudioClip mus)
+    {
+        if (!aus.clip.Equals(mus))
+        {
+            aus.Stop();
+            aus.clip = mus;
+            aus.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayMusicOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicOnLoad : MonoBehaviour
+{
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().PlayMusic(clip);
+    }
+}
