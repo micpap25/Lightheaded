4d47e23ce4bd77123e27ea969df2d4115e9e5bad
Melissa Ozcan <34689581+MelissaOzcan@users.noreply.github.com>
2020-06-27 11:08:24 -0400

    yellow balloon
    #blockyash

---
+++ Assets/Scripts/Balloon/YellowBalloon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBalloon : MonoBehaviour, Collectable
+{
+    public void FixedUpdate()
+    {
+        //visual stuff while not collected, floating up and down, particles
+    }
+    public void collide()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<PlayerController>().numBalloons++;
+        Destroy(gameObject);
+    }
+}
+
+// modify for yellow, this is just blue
