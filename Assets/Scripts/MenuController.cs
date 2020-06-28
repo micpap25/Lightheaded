5141845d5c50ad177e72e854d56ef521190353d8
Michael Papadopoulos <micpap25@gmail.com>
2020-06-28 15:21:32 -0400

    Update MenuController.cs

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -11,27 +11,12 @@ public class MenuController : MonoBehaviour
 {
     string path = "Assets/Resources/level.txt";
 
-
-  void Start () {
-
-    }
-    function Awake() {
-    allAudioSources = FindObjectsOfType(AudioSource) as AudioSource[];
-}
-
-function StopAllAudio() {
-    for(var audioS : AudioSource in allAudioSources) {
-        audioS.Stop();
-    }
-
   public void loadFirstLvl(){
       Debug.Log ("You have clicked the NewGameButton!");
-      StopAllAudio();
       SceneManager.LoadScene("Level1");
   }
   public void QuitGame(){
     Debug.Log("Haha loser you quit. jk lov u <3");
-    StopAllAudio();
     Application.Quit();
   }
 
@@ -44,13 +29,11 @@ function StopAllAudio() {
         inp_stm.Close();
 
         Debug.Log("resumed!");
-        StopAllAudio();
         SceneManager.LoadScene("Level" + maxlvl);
     }
 
     public void loadCredits(){
         Debug.Log ("You have clicked the CreditButton!");
-        StopAllAudio();
         SceneManager.LoadScene("Credit");
     }
     public void loadMenu(){
